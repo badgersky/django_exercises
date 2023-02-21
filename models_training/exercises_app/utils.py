2d85cb712a2766d8ab6a2e4e92f2f8e38892367e
badgersky <szymonek.ptasznik@gmail.com>
2023-02-22 00:24:04 +0100

    band data generator

--- models_training/exercises_app/utils.py
+++ models_training/exercises_app/utils.py
@@ -1,8 +1,18 @@
+from random import randint, choice
+
+
 def load_bands():
     file_name = '/home/badgersky/Desktop/bands.txt'
     with open(file_name, 'r') as f:
         bands = f.read()
         return bands.split(',')
+
+
+def generate_bands_data():
+    year = randint(1960, 2010)
+    genre = randint(0, 6)
+    still_active = choice(['True', 'False'])
+    return year, still_active, genre
 
 
 if __name__ == '__main__':
