e56392b5575cbfc40adcfe3ff9c5b2a5a2edd555
badgersky <szymonek.ptasznik@gmail.com>
2023-03-02 20:30:38 +0100

    created person and genre models, migrated changes, registered app

--- movie_service/manage_movies/models.py
+++ movie_service/manage_movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=32)
