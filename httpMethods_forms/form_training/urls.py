d3eba4a2152c699168793323182cdd61cf1fa186
badgersky <szymonek.ptasznik@gmail.com>
2023-03-04 15:44:57 +0100

    created temperature converter

--- httpMethods_forms/form_training/urls.py
+++ httpMethods_forms/form_training/urls.py
@@ -1,7 +1,8 @@
-from form_training.views import greet_user
+from form_training.views import greet_user, convert_temperature
 from django.urls import path
 
 
 urlpatterns = [
-    path('greetings/', greet_user)
+    path('greetings/', greet_user),
+    path('temp-converter/', convert_temperature),
 ]
--- httpMethods_forms/form_training/views.py
+++ httpMethods_forms/form_training/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 
 def greet_user(request):
@@ -9,3 +10,17 @@ def greet_user(request):
         surname = request.POST['surname']
         greetings = f'Hello {name.title()} {surname.title()}'
         return render(request, 'form_training/greet_user.html', context={'greetings': greetings})
+
+
+def convert_temperature(request):
+    if request.method == 'GET':
+        return render(request, 'form_training/temp_convector.html')
+    elif request.method == 'POST':
+        temp = float(request.POST['degrees'])
+        button = request.POST['convertionType']
+        if button == 'celcToFahr':
+            converted = temp * 1.8 + 32
+            return render(request, 'form_training/temp_convector.html', context={'temp': converted})
+        elif button == 'FahrToCelc':
+            converted = (temp - 32) * 0.5556
+            return render(request, 'form_training/temp_convector.html', context={'temp': converted})
