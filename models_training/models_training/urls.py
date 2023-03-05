e08ddad075c5eeaebcb400bef402b194b5089004
badgersky <szymonek.ptasznik@gmail.com>
2023-03-05 15:49:07 +0100

    created view that shows band data, band id in url

--- models_training/models_training/urls.py
+++ models_training/models_training/urls.py
@@ -20,6 +20,6 @@ from exercises_app.views import show_published_articles, show_band_data, AddBand
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', show_published_articles),
-    path('show-band/<int:band_id>', show_band_data),
+    path('show-band/<band_id>/', show_band_data),
     path('add-band/', AddBand.as_view()),
 ]
