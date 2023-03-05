14baff6432c262101e3b01bd66643ed5ab198bdf
badgersky <szymonek.ptasznik@gmail.com>
2023-03-05 10:33:05 +0100

    created views to add multiple cookies and show them all

--- cookies_sesions/cookies_exercises/urls.py
+++ cookies_sesions/cookies_exercises/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('set-cookie/', views.set_cookie),
     path('show-cookie/', views.view_cookie),
     path('delete-cookie/', views.delete_cookie),
+    path('set-multi-cookies/', views.add_to_cookie),
+    path('show-all-cookies/', views.view_all_cookies),
 ]
--- cookies_sesions/cookies_exercises/views.py
+++ cookies_sesions/cookies_exercises/views.py
@@ -21,3 +21,19 @@ def delete_cookie(request):
     if 'user' in request.COOKIES:
         response.delete_cookie('user')
         return response
+
+
+def add_to_cookie(request):
+    if request.method == 'GET':
+        return render(request, 'cookies_exercises/add_to_cookie.html')
+    if request.method == 'POST':
+        response = render(request, 'cookies_exercises/add_to_cookie.html')
+        cookie_name = request.POST['key']
+        cookie_value = request.POST['value']
+        response.set_cookie(cookie_name, cookie_value)
+        return response
+
+
+def view_all_cookies(request):
+    user_cookies = request.COOKIES
+    return render(request, 'cookies_exercises/show_all_cookies.html', context={'cookies': user_cookies})
