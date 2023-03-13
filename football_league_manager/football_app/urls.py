4c417b8af420e66d79694895dab9c9cee2b13c0d
badgersky <szymonek.ptasznik@gmail.com>
2023-03-13 17:53:16 +0100

    created view that shows all games played by certain team

--- football_league_manager/football_app/urls.py
+++ football_league_manager/football_app/urls.py
@@ -1,7 +1,8 @@
-from football_app.views import league_table
+from football_app.views import league_table, games_played
 from django.urls import path
 
 
 urlpatterns = [
     path('table/', league_table),
+    path('games/<team_id>/', games_played)
 ]
--- football_league_manager/football_app/views.py
+++ football_league_manager/football_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from football_app.models import Team
+from football_app.models import Team, Game
 from django.http import HttpResponse
 
 
@@ -7,3 +7,13 @@ def league_table(request):
     teams = Team.objects.all().order_by('points').reverse()
     teams_dict = {pos: team for pos, team in enumerate(teams, start=1)}
     return render(request, 'football_app/view_league_table.html', {'teams': teams_dict})
+
+
+def games_played(request, team_id):
+    games_home = Game.objects.filter(team_home_id=team_id)
+    games_away = Game.objects.filter(team_away_id=team_id)
+    return render(request, 'football_app/view_played_games.html', {
+        'games_home': games_home,
+        'games_away': games_away,
+        })
+
