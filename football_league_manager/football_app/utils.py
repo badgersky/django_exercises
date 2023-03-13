ccd99ec4cc576b448d4a784170fd135f9804d352
badgersky <szymonek.ptasznik@gmail.com>
2023-03-13 13:52:49 +0100

    inserting data to table

--- football_league_manager/football_app/utils.py
+++ football_league_manager/football_app/utils.py
@@ -2,7 +2,7 @@ from football_app.models import Game, Team
 
 
 def load_games_data():
-    filename = 'games_teams_data/games.txt'
+    filename = '/home/badgersky/workspace/django_exercises/football_league_manager/football_app/games_teams_data/games.txt'
     with open(filename, 'r') as f:
         lines = f.readlines()
         new_lines = []
@@ -13,7 +13,7 @@ def load_games_data():
 
 
 def load_teams_data():
-    filename = 'games_teams_data/teams.txt'
+    filename = '/home/badgersky/workspace/django_exercises/football_league_manager/football_app/games_teams_data/teams.txt'
     with open(filename, 'r') as f:
         lines = f.readlines()
         new_lines = []
@@ -21,8 +21,23 @@ def load_teams_data():
             line = line.rstrip('\n')
             new_lines.append([line.split(', ')[0], int(line.split(', ')[1])])
         return new_lines
-    
+
+
+def insert_teams_data():
+    for team in load_teams_data():
+        Team.objects.create(name=team[0], points=team[1])
+
+
+def insert_game_data():
+    for game in load_games_data():
+        Game.objects.create(
+            team_home_id=game[0],
+            team_home_goals=game[1],
+            team_away_id=game[2],
+            team_away_goals=game[3]
+            )
+
 
 if __name__ == '__main__':
-    print(load_games_data())
-    print(load_teams_data())
+    insert_game_data()
+    insert_teams_data()
